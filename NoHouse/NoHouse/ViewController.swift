3ccbabed4e617bb90f103c2a3611ee4e54a773cd
Josh Cobert <josh@Joshs-iBook.local>
2018-02-09 00:50:57 -0500

    More refinements to login system

--- NoHouse/NoHouse/ViewController.swift
+++ NoHouse/NoHouse/ViewController.swift
@@ -10,52 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let userLoginList : Array = ["josh","joe","mbl317"]
-    let pwdLoginList : Array = ["jc","jm","ml"]
-    let userList : Array = ["Josh","Joe","Mike"]
+    let usrLoginList = ["josh","joe","mbl317"]
+    let pwdLoginList = ["jc","jm","ml"]
+    let userFirstName = ["Josh","Joe","Mike"]
     
     @IBOutlet weak var txtUsername: UITextField!
     @IBOutlet weak var txtPassword: UITextField!    
     @IBOutlet weak var lblTestMessage: UILabel!
-    @IBOutlet weak var rememberMeToggle: UISwitch!
+    @IBOutlet weak var tglRemember: UISwitch!
     
-    var currentUser : Int = 0
+    var currentUserIndex : Int = 0
     var loginText : String = ""
+    var rememberMe : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        lblTestMessage.text = ""
         
 // Adds ability to dismiss keyboard
         let kbdToolbar = UIToolbar()
         let kbdDoneButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.done, target: self, action: #selector(self.kbdDone_Click))
         let flexSpace = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.flexibleSpace, target: nil, action: nil)
-        
         kbdToolbar.setItems([flexSpace, kbdDoneButton], animated: false)
         kbdToolbar.sizeToFit()
-        
         txtUsername.inputAccessoryView = kbdToolbar
         txtPassword.inputAccessoryView = kbdToolbar
+        
+// Sets login fields to previous values, if remember me toggle was on prior to last login
+        if rememberMe == true {
+            txtUsername.text = usrLoginList[currentUserIndex]
+            txtPassword.text = pwdLoginList[currentUserIndex]
+            tglRemember.isOn = true
+        }
     }
-    
     @objc func kbdDone_Click() {
         view.endEditing(true)
     }
     
+// Prepares values to send through segue to home page
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "loginToMainPage" {
+        if segue.identifier == "loginToHomePage" {
             let nextView = segue.destination as! ViewControllerTwo
             nextView.loginText = loginText
+            nextView.rememberStatus = tglRemember.isOn
+            nextView.currentUser = currentUserIndex
         }
     }
     
 // Login button action
     @IBAction func btnLogin_Click(_ sender: Any) {
         
-        if let username = txtUsername.text, let password = txtPassword.text, let usrInt = userLoginList.index(of: username), let pwdInt = pwdLoginList.index(of: password) {
+        if let username = txtUsername.text, let password = txtPassword.text, let usrInt = usrLoginList.index(of: username), let pwdInt = pwdLoginList.index(of: password) {
             if usrInt == pwdInt {
-                currentUser = userLoginList.index(of: username)!
-                loginText = "Welcome \(userList[currentUser])!"
-                performSegue(withIdentifier: "loginToMainPage", sender: loginText)
+                currentUserIndex = usrLoginList.index(of: username)!
+                loginText = "Welcome \(userFirstName[currentUserIndex])!"
+                performSegue(withIdentifier: "loginToHomePage", sender: loginText)
             }
         } else {
             lblTestMessage.text = "Invalid login"
--- NoHouse/NoHouse/ViewControllerTwo.swift
+++ NoHouse/NoHouse/ViewControllerTwo.swift
@@ -12,7 +12,14 @@ class ViewControllerTwo: UIViewController {
     
     @IBOutlet weak var btnLogout: UIButton!
     @IBOutlet weak var lblWelcome: UILabel!
+    @IBOutlet weak var lblBalance: UILabel!
+    @IBOutlet weak var homePageTabBar: UITabBar!
+    @IBOutlet weak var tabBar1: UITabBarItem!
+    @IBOutlet weak var tabBar2: UITabBarItem!
+    
     var loginText : String = ""
+    var rememberStatus : Bool = false
+    var currentUser : Int = 0
     
     
    
@@ -20,13 +27,21 @@ class ViewControllerTwo: UIViewController {
         super.viewDidLoad()
 
         lblWelcome.text = loginText
+        homePageTabBar.selectedItem = tabBar1
         
     }
 
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "logout" {
+            let nextView = segue.destination as! ViewController
+            nextView.rememberMe = rememberStatus
+            nextView.currentUserIndex = currentUser
+        }
+    }
     
     @IBAction func btnLogout_Click(_ sender: Any) {
         
-        performSegue(withIdentifier: "logout", sender: self)
+        performSegue(withIdentifier: "logout", sender: rememberStatus)
         
     }
     
